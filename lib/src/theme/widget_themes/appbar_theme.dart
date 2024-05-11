475029f50df00b31330ce4485e381f4be2555c64
Qasim-afzaal <147349016+Qasim-afzaal@users.noreply.github.com>
2024-05-11 16:08:56 +0500

    Add custom appbar

---
+++ lib/src/theme/widget_themes/appbar_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ProjectAppBarTheme {
+  ProjectAppBarTheme._();
+
+  static AppBarTheme lightAppBarTheme = const AppBarTheme(
+    elevation: 0,
+    centerTitle: false,
+    iconTheme: IconThemeData(
+      size: 18.0,
+    ),
+    actionsIconTheme: IconThemeData(
+      size: 18.0,
+    ),
+  );
+
+  static AppBarTheme darkAppBarTheme = const AppBarTheme(
+    elevation: 0,
+    centerTitle: false,
+    iconTheme: IconThemeData(
+      size: 18.0,
+    ),
+    actionsIconTheme: IconThemeData(
+      size: 18.0,
+    ),
+  );
+}
