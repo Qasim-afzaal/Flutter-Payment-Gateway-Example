3f74da7338606398516b50af8553db388ab4ae8a
Qasim-afzaal <147349016+Qasim-afzaal@users.noreply.github.com>
2024-05-11 16:06:20 +0500

    Custom size for theme

---
+++ lib/src/theme/extension_themes/custom_sizes_theme.dart
@@ -0,0 +1,17 @@
+import 'package:paymentgetwaysample/src/theme/extensions/custom_sizes.dart';
+
+class CustomSizesTheme {
+  CustomSizesTheme._();
+
+  static const CustomSizes lightTheme = CustomSizes(
+    padding: 20,
+    borderRadius: 15,
+    formHeight: 30,
+  );
+
+  static const CustomSizes darkTheme = CustomSizes(
+    padding: 20,
+    borderRadius: 15,
+    formHeight: 30,
+  );
+}
