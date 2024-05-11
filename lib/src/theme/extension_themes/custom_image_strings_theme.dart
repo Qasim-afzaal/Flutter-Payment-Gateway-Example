7c5a692c73b32d21dcaa39d1972dab029ba49a6a
Qasim-afzaal <147349016+Qasim-afzaal@users.noreply.github.com>
2024-05-11 16:06:09 +0500

    Theming in App

---
+++ lib/src/theme/extension_themes/custom_image_strings_theme.dart
@@ -0,0 +1,13 @@
+import 'package:paymentgetwaysample/src/theme/extensions/custom_image_strings.dart';
+
+class CustomImageStringsTheme {
+  CustomImageStringsTheme._();
+
+  static const CustomImageStrings lightTheme = CustomImageStrings(
+    conviergeLogo: 'assets/images/logo-purple.png',
+  );
+
+  static const CustomImageStrings darkTheme = CustomImageStrings(
+    conviergeLogo: 'assets/images/logo.png',
+  );
+}
