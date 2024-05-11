8c81fe7b31110fc49364484b1c13d410d55c6f17
Qasim-afzaal <147349016+Qasim-afzaal@users.noreply.github.com>
2024-05-11 16:01:55 +0500

    Add globle var

---
+++ lib/src/config/globle/globle.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+final GlobalKey<ScaffoldMessengerState> snackbarKey =
+    GlobalKey<ScaffoldMessengerState>();
+final GlobalKey<FormState> signupFormKey = GlobalKey<FormState>();
+
+// TODO: This needs to be refactored out of global scope and properly tracked
+
